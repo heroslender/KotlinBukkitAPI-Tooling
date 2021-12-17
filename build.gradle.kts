5b92df900896f817ced1e2046e247e751965dce9
heroslender <admin@heroslender.com>
2021-12-17 21:00:42 +0000

    Update gradle plugins
    - Bump gradle-wrapper from 6.1.1 to 7.3.2
    - Bump kotlin from 1.4.0 to 1.6.10
    - Bump org.jetbrains.intellij from 0.4.21 to 1.3.0
    - Bump org.jetbrains.changelog from 1.1.2 to 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// Get a property from the `gradle.properties` file
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     // Java support
     java
     // Kotlin support
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "0.4.21"
+    id("org.jetbrains.intellij") version "1.3.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.1.2"
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
-val kotlinVersion = "1.4.0"
 val kotlinBukkitAPIVersion = "0.2.0-SNAPSHOT"
 val bukkriptVersion = "0.2.0-SNAPSHOT"
 
@@ -24,7 +24,7 @@ version = "0.0.7"
 repositories {
     jcenter()
     mavenLocal()
-    maven("http://nexus.devsrsouza.com.br/repository/maven-public/")
+    maven("https://nexus.devsrsouza.com.br/repository/maven-public/")
 }
 
 dependencies {
@@ -33,28 +33,28 @@ dependencies {
     val changing = Action<ExternalModuleDependency> { isChanging = true }
     api("br.com.devsrsouza.bukkript:script-definition-embedded:$bukkriptVersion", changing)
 
-    testCompile("junit", "junit", "4.12")
+    testCompileOnly("junit", "junit", "4.12")
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    setPlugins(
-        *properties("platformPlugins")
+    plugins.set(
+        properties("platformPlugins")
             .split(',')
             .map(String::trim)
             .filter(String::isNotEmpty)
-            .toTypedArray()
     )
 }
+
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    version = properties("pluginVersion")
-    groups = emptyList()
+    version.set(properties("pluginVersion"))
+    groups.set(emptyList())
 }
 
 tasks {
@@ -74,45 +74,46 @@ tasks {
     }
 
     patchPluginXml {
-        setVersion(properties("pluginVersion"))
-        setSinceBuild(properties("pluginSinceBuild"))
-        setUntilBuild(properties("pluginUntilBuild"))
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
 
-        setPluginDescription(
+        pluginDescription.set(
             """
         <img src="https://github.com/DevSrSouza/KotlinBukkitAPI/raw/master/logo.png" width="417" height="161"/>
-        
+
         <br />
 
-        The KotlinBukkitAPI Tooling is plugin for IntelliJ that helps 
+        The KotlinBukkitAPI Tooling is plugin for IntelliJ that helps
         <br />
         developers using <a href="https://github.com/DevSrSouza/KotlinBukkitAPI">KotlinBukkitAPI</a> and scripts for <a href="https://github.com/DevSrSouza/Bukkript">Bukkript</a>.
         <br />
         This libraries help build extensions for Minecraft Server using Spigot server.
-        
+
         <br />
-        
+
         <br />
-        
+
         <ul>
         <li><a href='https://github.com/DevSrSouza/Bukkript'>Bukkript</a></li>
         <li<a href='https://github.com/DevSrSouza/KotlinBukkitAPI-Tooling'>KotlinBukkitAPI-Tooling (this plugin)</a></li>
         <li<a href='https://github.com/DevSrSouza/KotlinBukkitAPI'>KotlinBukkitAPI</a></li>
         <li><a href='https://github.com/KotlinMinecraft/KotlinBukkitAPI-Examples'>KotlinBukkitAPI Examples</a></li>
         </ul>
-        
+
         <br />
         <br />
-        
+
         <h3>Demonstration</h3>
-        
+
         <br />
-        
+
         <img src="https://i.imgur.com/exlwVUs.gif" width='680' height='390'/>
     """.trimIndent()
         )
 
-        setChangeNotes(
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(
             changelog.run {
                 kotlin.runCatching { get(properties("pluginVersion")) }.getOrElse { getLatest() }
             }.toHTML()
@@ -120,7 +121,7 @@ tasks {
     }
 
     publishPlugin {
-        token(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
+        token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
     }
 }
 
